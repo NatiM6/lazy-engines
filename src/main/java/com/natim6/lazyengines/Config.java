c0902002eeebf937ab138aa458acd57efdc0a154
NatiM6 <natanekm@gmail.com>
2024-06-10 17:40:45 +0200

    Added so much config it warrants a major update

---
+++ src/main/java/com/natim6/lazyengines/Config.java
@@ -0,0 +1,27 @@
+package com.natim6.lazyengines;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = "lazyengines", bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config {
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    public static final ForgeConfigSpec.EnumValue<PassiveConfig> NON_BURNER = BUILDER
+            .comment("Heat level of a passive non-burner source")
+            .defineEnum("passive_source_mode", PassiveConfig.DISABLED);
+    public static final ForgeConfigSpec.EnumValue<PassiveConfig> PASSIVE_BURNER = BUILDER
+            .comment("Heat level of a passive burner")
+            .defineEnum("passive_burner_mode", PassiveConfig.DISABLED);
+    public static final ForgeConfigSpec.IntValue KINDLED_BURNER = BUILDER
+            .comment("Heat level of a kindled burner")
+            .defineInRange("kindled_burner_heat", 1, 1, Integer.MAX_VALUE);
+    public static final ForgeConfigSpec.IntValue SEETHING_BURNER = BUILDER
+            .comment("Heat level of a seething burner")
+            .defineInRange("seething_burner_heat", 4, 2, Integer.MAX_VALUE);
+    public static final ForgeConfigSpec.IntValue TANKS_PER_HEAT = BUILDER
+            .comment("Amount of tanks required to support each level of heat")
+            .defineInRange("tanks_per_heat_level", 4, 1, Integer.MAX_VALUE);
+
+    static final ForgeConfigSpec SPEC = BUILDER.build();
+}
--- src/main/java/com/natim6/lazyengines/LazyEngines.java
+++ src/main/java/com/natim6/lazyengines/LazyEngines.java
@@ -1,9 +1,13 @@
 package com.natim6.lazyengines;
 
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod("lazyengines")
 public class LazyEngines
 {
-    public LazyEngines() {}
+    public LazyEngines() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SPEC);
+    }
 }
