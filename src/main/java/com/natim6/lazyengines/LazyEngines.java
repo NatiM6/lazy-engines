727f0672d42563f98f95fbc8c4db4de9807b606b
NatiM6 <natanekm@gmail.com>
2024-06-10 19:48:56 +0200

    Major fabric port

--- src/main/java/com/natim6/lazyengines/LazyEngines.java
+++ src/main/java/com/natim6/lazyengines/LazyEngines.java
@@ -1,11 +1,13 @@
 package com.natim6.lazyengines;
 
+import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
 import net.fabricmc.api.ModInitializer;
+import net.minecraftforge.fml.config.ModConfig;
 
 public class LazyEngines implements ModInitializer {
 
     @Override
     public void onInitialize() {
-
+        ForgeConfigRegistry.INSTANCE.register("lazyengines", ModConfig.Type.SERVER, Config.SPEC);
     }
 }
--- src/main/java/com/natim6/lazyengines/mixin/BoilerHeatersMixin.java
+++ src/main/java/com/natim6/lazyengines/mixin/BoilerHeatersMixin.java
@@ -1,8 +1,11 @@
 package com.natim6.lazyengines.mixin;
 
+import com.natim6.lazyengines.Config;
+import com.simibubi.create.AllTags;
 import com.simibubi.create.content.fluids.tank.BoilerHeaters;
 import com.simibubi.create.content.processing.burner.BlazeBurnerBlock;
 import com.simibubi.create.content.processing.burner.BlazeBurnerBlock.HeatLevel;
+import com.simibubi.create.foundation.utility.BlockHelper;
 import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,13 +19,21 @@ public class BoilerHeatersMixin {
     private static void registerDefaultsMixin(CallbackInfo ci) {
         registerHeater(com.simibubi.create.AllBlocks.BLAZE_BURNER.get(), (level, pos, state) -> {
             HeatLevel value = state.getValue(BlazeBurnerBlock.HEAT_LEVEL);
-            if (value == HeatLevel.SEETHING) return 4;
-            if (value == HeatLevel.KINDLED) return 1;
-            if (value == HeatLevel.FADING) return 0;
-            return -1;
+            if (value == HeatLevel.SEETHING) return Config.SEETHING_BURNER.get();
+            if (value.isAtLeast(HeatLevel.FADING)) return Config.KINDLED_BURNER.get();
+            return Config.PASSIVE_BURNER.get().value();
         });
+
+        registerHeaterProvider((level, pos, state) -> {
+            if (AllTags.AllBlockTags.PASSIVE_BOILER_HEATERS.matches(state) && BlockHelper.isNotUnheated(state)) {
+                return (level1, pos1, state1) -> Config.NON_BURNER.get().value();
+            }
+            return null;
+        });
+
         ci.cancel();
     }
 
     @Shadow public static void registerHeater(Block block, BoilerHeaters.Heater heater) {}
+    @Shadow public static void registerHeaterProvider(BoilerHeaters.HeaterProvider provider) {}
 }
