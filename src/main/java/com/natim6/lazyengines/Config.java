31fba4b8fe6ca37d76525e81a6d7e441bf8447e3
NatiM6 <natanekm@gmail.com>
2024-06-10 18:13:48 +0200

    Changed the config to default

--- src/main/java/com/natim6/lazyengines/Config.java
+++ src/main/java/com/natim6/lazyengines/Config.java
@@ -9,16 +9,16 @@ public class Config {
 
     public static final ForgeConfigSpec.EnumValue<PassiveConfig> NON_BURNER = BUILDER
             .comment("Heat level of a passive non-burner source")
-            .defineEnum("passive_source_mode", PassiveConfig.DISABLED);
+            .defineEnum("passive_source_mode", PassiveConfig.PASSIVE);
     public static final ForgeConfigSpec.EnumValue<PassiveConfig> PASSIVE_BURNER = BUILDER
             .comment("Heat level of a passive burner")
-            .defineEnum("passive_burner_mode", PassiveConfig.DISABLED);
+            .defineEnum("passive_burner_mode", PassiveConfig.PASSIVE);
     public static final ForgeConfigSpec.IntValue KINDLED_BURNER = BUILDER
             .comment("Heat level of a kindled burner")
             .defineInRange("kindled_burner_heat", 1, 1, Integer.MAX_VALUE);
     public static final ForgeConfigSpec.IntValue SEETHING_BURNER = BUILDER
             .comment("Heat level of a seething burner")
-            .defineInRange("seething_burner_heat", 4, 2, Integer.MAX_VALUE);
+            .defineInRange("seething_burner_heat", 2, 2, Integer.MAX_VALUE);
     public static final ForgeConfigSpec.IntValue TANKS_PER_HEAT = BUILDER
             .comment("Amount of tanks required to support each level of heat")
             .defineInRange("tanks_per_heat_level", 4, 1, Integer.MAX_VALUE);
